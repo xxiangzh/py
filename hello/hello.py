cedb811e9381ba7049b63c07385144b4de5694e2
xzh <anenan@qq.com>
2024-10-12 18:26:47 +0800

    hello world4

--- hello/hello.py
+++ hello/hello.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import hellocp
+
+
 def hl(x):
     print("hello world", x)
 
@@ -8,4 +11,8 @@ def hl(x):
 # 程序主入口
 if __name__ == "__main__":
     print("hello world")
+
     hl('张三')
+
+    c = hellocp.Hlclass('李四', 3)
+    c.hlh()
