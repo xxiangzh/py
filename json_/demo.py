42f5d9bb646d88d639bd88b496151328efffb059
xzh <anenan@qq.com>
2024-10-14 14:12:10 +0800

    json demo

---
+++ json_/demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import json
+
+# 程序主入口
+if __name__ == "__main__":
+    # 定义python对象
+    pythonObj = {
+        'no': 1,
+        'name': '哈啊'
+    }
+    print("type：", type(pythonObj))
+    print("pythonObj：", pythonObj)
+
+    # python字典 转 json字符串
+    jsonStr = json.dumps(pythonObj)
+    print("type：", type(jsonStr))
+    print("jsonStr：", jsonStr)
+
+    # json字符串 转 python字典
+    pythonDict = json.loads(jsonStr)
+    print("type：", type(pythonDict))
+    print("pythonDict：", pythonDict)
+    print("pythonDict：", repr(pythonDict))
