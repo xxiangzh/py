0e19bb5c2a2a7e0bed9f321879244161879139a5
xzh <anenan@qq.com>
2024-10-14 16:20:29 +0800

    mysql demo2

--- mysql_/mysql_demo.py
+++ mysql_/mysql_demo.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import mysql.connector
 
 # 程序主入口
 if __name__ == "__main__":
 
     # 建立数据库连接
-    mydb = mysql.connector.connect(
+    dbc = mysql.connector.connect(
         host="localhost",
         user="root",
         passwd="123456",
         database="xzh"
     )
-    print(mydb)
+    print(dbc)
 
-    cursor = mydb.cursor()
+    cursor = dbc.cursor()
 
     sql = "SELECT * FROM t_user WHERE id = 1"
 
@@ -22,4 +25,8 @@ if __name__ == "__main__":
 
     for x in result:
         print(x)
+
+    # 关闭游标和连接
+    cursor.close()
+    dbc.close()
 
---
+++ mysql_/pymysql_demo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pymysql
+
+# 程序主入口
+if __name__ == "__main__":
+
+    # 建立数据库连接
+    dbc = pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="123456",
+        database="xzh"
+    )
+    print(dbc)
+
+    cursor = dbc.cursor()
+
+    sql = "SELECT * FROM t_user WHERE id = 1"
+
+    cursor.execute(sql)
+
+    result = cursor.fetchall()
+
+    for x in result:
+        print(x)
+
+    # 关闭游标和连接
+    cursor.close()
+    dbc.close()
